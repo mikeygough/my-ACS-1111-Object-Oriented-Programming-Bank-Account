a78b5be8ed2fc7b06f1d72edb3345728991809c0
mikey <mikey@hey.com>
2023-10-25 17:11:10 -0500

    add account_type. adjust add_interest to incorporate checking/savings

--- BankAccount.py
+++ BankAccount.py
@@ -4,10 +4,12 @@ class BankAccount:
     """ BankAccount stores full_name, account_number (randomly generated 8 digit number) and a balance """
     def __init__(self, full_name: str = '', 
                        account_number = randint(10000000, 99999999),
-                       balance: float = 0.0):
+                       balance: float = 0.0,
+                       account_type: str = 'checking'):
         self.full_name = full_name
         self.account_number = account_number
         self.balance = balance
+        self.account_type = account_type
         
     def deposit(self, amount: float):
         """ increase balance by the amount. """
@@ -33,11 +35,14 @@ class BankAccount:
     def add_interest(self):
         """ if account balance > 0 then add 0.083% interest to the account balance. """
         if self.balance >= 0:
-            self.balance += round(self.balance * 0.00083, 2)
+            if self.account_type == 'checking':
+                self.balance += round(self.balance * 0.00083, 2)
+            elif self.account_type == 'savings':
+                self.balance += round(self.balance * 0.001, 2)
             print(f"Adding Interest")
         else:
             print("Insufficient Funds. Accounts with negative balances cannot accrue interest")
-            self.print_balance()
+        self.print_balance()
     
     def print_statement(self):
         """ prints the account full_name, account_number and balance. """
@@ -49,26 +54,54 @@ class BankAccount:
         """ prints the account balance (formatted). """
         print(f"Balance: ${self.balance:.2f}")
     
-# TESTING
-
-# INIT
-account = BankAccount(full_name="Odysseus X", balance=100)
-print("Full Name:", account.full_name)
-print("Account Number:", account.account_number)
-print("Balance:", account.balance)
-
+# TEST 1
+odysseus_x = BankAccount(full_name="Odysseus X", balance=100, account_type='savings')
+print("Full Name:", odysseus_x.full_name)
+print("Account Number:", odysseus_x.account_number)
+print("Balance:", odysseus_x.balance)
 # DEPOSIT
-account.deposit(50) # -> balance = 150
-
+odysseus_x.deposit(50) # -> balance = 150
 # WITHDRAW
-account.withdraw(25) # -> balance = 125
-# account.withdraw(126) # -> "Insufficient Funds. Charging $10 overdraft fee."
-
+odysseus_x.withdraw(25) # -> balance = 125
 # GET_BALANCE
-account.get_balance() # -> "Current balance: $125". Returns self.balance
-
+odysseus_x.get_balance() # -> "Current balance: $125". Returns self.balance
 # ADD_INTEREST
-account.add_interest() # -> "Adding Interest\n Current balance: $125.10375". balance = 125.10375
-
+odysseus_x.add_interest() # -> "Adding Interest\n Current balance: $125.10375". balance = 125.10375
 # PRINT_STATEMENT
-account.print_statement() # -> "Odysseus X" "Account No.: ****3285" "Balance: $125.10375"
+odysseus_x.print_statement() # -> "Odysseus X" "Account No.: ****3285" "Balance: $125.10375"
+
+# TEST 2
+bart_s = BankAccount(full_name="Bart Simpson", balance=50)
+print("Full Name:", bart_s.full_name)
+print("Account Number:", bart_s.account_number)
+print("Balance:", bart_s.balance)
+# DEPOSIT
+bart_s.deposit(5)
+# WITHDRAW
+bart_s.withdraw(100) # Insufficient Funds. Charging $10 overdraft fee.
+# GET_BALANCE
+bart_s.get_balance()
+# ADD_INTEREST
+bart_s.add_interest()
+# PRINT_STATEMENT
+bart_s.print_statement()
+
+# TEST 3
+# Create a new bank account instance: user: "Mitchell", account number: 03141592.
+mitchell_h = BankAccount(full_name="Mitchell Hudson", account_number='03141592')
+# Deposit $400,000 into "Mitchell's" account.
+mitchell_h.deposit(400000)
+# Print a statement
+mitchell_h.print_statement()
+# Add interest to the account
+mitchell_h.add_interest()
+# Print a statement
+mitchell_h.print_statement()
+# Make a withdrawl of $150 (Mitchell needs some Yeezy's)
+mitchell_h.withdraw(150)
+# Print a statement
+mitchell_h.print_statement()
+
+bank = [odysseus_x, bart_s, mitchell_h]
+for account in bank:
+    account.add_interest()
